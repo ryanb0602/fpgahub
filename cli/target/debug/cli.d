5154d3f76717a429ef94ecbbc8b9c7c2b74484dc
Ryan Berube <berubr@rpi.edu>
2025-09-17 19:17:29 -0400

    Changing from rust to c++ for cli

--- cli/target/debug/cli.d
+++
@@ -1,1 +0,0 @@
-/home/ryanb/FPGAhub/cli/target/debug/cli: /home/ryanb/FPGAhub/cli/src/main.rs
--- cli/target/debug/deps/cli-f6219c26bf2251b9.d
+++
@@ -1,5 +0,0 @@
-/home/ryanb/FPGAhub/cli/target/debug/deps/cli-f6219c26bf2251b9.d: src/main.rs
-
-/home/ryanb/FPGAhub/cli/target/debug/deps/cli-f6219c26bf2251b9: src/main.rs
-
-src/main.rs:
