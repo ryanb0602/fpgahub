0ceb4eb7e3b2dfd093c7527fe8c9b70b068c2f03
Ryan Berube <berubr@rpi.edu>
2025-09-17 18:52:12 -0400

    login route made

--- backend/src/authroutes.js
+++ backend/src/authroutes.js
@@ -34,11 +34,46 @@ router.post("/register", async (req, res) => {
 
 		res.status(201).json({
 			message: "User registered successfully.",
-			user: resulttest.rows[0],
 		});
 	} catch (err) {
 		console.log(err);
 	}
+});
+
+router.post("/login", async (req, res) => {
+	const { email, password } = req.body;
+	let uuid = null;
+
+	try {
+		const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+			email,
+		]);
+		const users = result.rows;
+
+		if (users.length < 1 || users.length > 1) {
+			return res.status(401).json({ message: "Invalid email or password" });
+		}
+
+		const user = users[0];
+
+		if (user.password === "") {
+			return res
+				.status(401)
+				.json({ message: "User account has yet to be activated" });
+		}
+
+		const valid = await bcrypt.compare(password, user.password);
+		if (!valid) {
+			return res.status(401).json({ message: "Invalid email or password" });
+		}
+
+		uuid = user.id;
+	} catch (err) {
+		console.log(err);
+	}
+
+	//req.session.userId = uuid;
+	res.json({ message: "Logged in successfully" });
 });
 
 module.exports = router;
