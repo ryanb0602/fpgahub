7fbfeb4919db9387446bd0a0ae69addbb9c50fd1
Ryan Berube <berubr@rpi.edu>
2026-04-06 17:26:45 -0400

    removed some dev tools

--- query_db.js
+++
@@ -1,9 +0,0 @@
-const pool = require('./backend/src/db');
-async function query() {
-  const nodes = await pool.query('SELECT DISTINCT module FROM (SELECT unnest(modules) AS module FROM files) t');
-  console.log("NODES:", nodes.rows);
-  const edges = await pool.query('SELECT parent_module, child_module FROM edges');
-  console.log("EDGES:", edges.rows);
-  process.exit(0);
-}
-query();
