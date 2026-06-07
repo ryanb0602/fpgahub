4ebde9999338ba531772b6777a6f391cb4f9dd31
Ryan Berube <berubr@rpi.edu>
2026-06-07 14:44:43 -0400

    ok some fixes that acttually get the linker working :

--- cli/build.sh
+++ cli/build.sh
@@ -4,15 +4,16 @@ git submodule update --init --recursive
 
 cd ./yosys
 
-# Toggled the custom Yosys library flag to ON
 cmake -B build . -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DYOSYS_INSTALL_LIBRARY=ON
 cmake --build build --config Release --parallel $(nproc)
 sudo cmake --install build --strip
 
 cd ..
 
-# Compile and link. (If it fails to find -lyosys, swap -L/usr/local/lib with -L/usr/local/lib64)
-g++ main.cpp ./src/*.cpp -I./include $(yosys-config --cxxflags) $(yosys-config --ldflags) -o fpgahub -L/usr/local/lib -lyosys 
+sudo mv /usr/local/lib/yosys/libyosys.so /usr/local/lib/
+sudo ldconfig
+
+g++ main.cpp ./src/*.cpp -I./include $(yosys-config --cxxflags) $(yosys-config --ldflags) -o fpgahub -L/usr/local/lib -L/usr/local/lib64 -Wl,-rpath,/usr/local/lib -Wl,-rpath,/usr/local/lib64 -lyosys
 
 sudo mv ./fpgahub /usr/local/bin/fpgahub
 sudo chmod +x /usr/local/bin/fpgahub
